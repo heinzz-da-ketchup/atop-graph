75a16abb97ee60bfe7491a67206c534ce55466cd
Jan Hrubes <jan.hrubes@nic.cz>
2022-11-07 13:06:36 +0100

    Change filenames for parsed output files

--- docker-atop-convert/convert.sh
+++ docker-atop-convert/convert.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 if [ x$TASK = xCONVERT ] ; then
-    for i in `find $SRC_PATH -type f -name \* ` ; do
+    for i in `find $SRC_PATH -type f ! -name .keepit` ; do
         filename=$(basename "$i")
-        UUID=$(cat /proc/sys/kernel/random/uuid)
         echo -n "Processing $filename..."
-        atop -r $i -P ALL |grep -v '^PR' |awk -f /parse.awk >$DST_PATH/atop-$UUID.txt;
-        echo " Done. Result saved to atop-$UUID.txt"
+        atop -r $i -P ALL |grep -v '^PR' |awk -f /parse.awk >$DST_PATH/$filename-parsed.txt
+        echo " Done. Result saved to $filename-parsed.txt"
     done
 elif [ x$TASK = xPUSH ] ; then
-    for i in `find $DST_PATH -type f -name atop\*.txt ` ; do
+    for i in `find $DST_PATH -type f -name *-parsed.txt` ; do
         echo "Importing $i to influxdb.."
         influx -import -path=$i -precision=s -host $IP || echo "Error importing $i"
     done
